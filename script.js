c4b73d837e770fcca57848c1bb9bb69b358e2544
Truong Thanh Loi <thanhloi8855@gmail.com>
2021-10-19 17:05:02 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let video = document.getElementById('webcam');
+let text = document.getElementById("text");
+let model;
+const names = ['Đeo', 'Đeo sai', 'Không đeo']
+
+const setupCamera = () =>
+{
+  navigator.mediaDevices.getUserMedia(
+    {
+      video: {width:640, height: 480},
+      audio: false,
+    }
+  ).then((stream) => {
+    video.srcObject = stream;
+  });
+};
+
+
+  detectMask = async (model) =>{
+  tf.engine().startScope();
+  const input = tf.tidy(() => {
+    return tf.image.resizeBilinear(tf.browser.fromPixels(video), [640, 640])
+      .div(255.0).expandDims(0);
+  });
+  model.executeAsync(input).then(res => {
+    
+    const [boxes, scores, classes, valid_detections] = res;
+    const boxes_data = boxes.dataSync();
+    const scores_data = scores.dataSync();
+    const classes_data = classes.dataSync();
+    const valid_detections_data = valid_detections.dataSync()[0];
+
+    tf.dispose(res)
+    var i;
+
+      for (i = 0; i < valid_detections_data; ++i)
+        text.innerHTML = names[classes_data[i]];
+        requestAnimationFrame(() => {
+          this.detectMask(model);
+        });    
+        tf.engine().endScope();
+  })
+}
+
+    setupCamera();  
+    video.addEventListener("loadeddata", async()=>{
+      model = await tf.loadGraphModel("https://raw.githubusercontent.com/Rimosolia/Mask-face-js/master/model.json");
+      detectMask(model);
+    });
+
